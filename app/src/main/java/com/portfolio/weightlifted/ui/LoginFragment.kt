06330863538d0aebe0da53a4d82da257e24cc3ee
Joshua Singleton <jsingle345@gmail.com>
2023-03-22 00:05:21 -0400

    Added login fragment to Activity

--- app/src/main/java/com/portfolio/weightlifted/ui/LoginFragment.kt
+++ app/src/main/java/com/portfolio/weightlifted/ui/LoginFragment.kt
@@ -6,7 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.portfolio.weightlifted.R
+import com.portfolio.weightlifted.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
+    lateinit var loginBinding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        loginBinding = FragmentLoginBinding.inflate(layoutInflater, container, false)
+        return loginBinding.root
+    }
 }
--- app/src/main/java/com/portfolio/weightlifted/ui/MainActivity.kt
+++ app/src/main/java/com/portfolio/weightlifted/ui/MainActivity.kt
@@ -2,11 +2,21 @@ package com.portfolio.weightlifted.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.portfolio.weightlifted.R
+import com.portfolio.weightlifted.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(applicationContext))
+        setContentView(binding.root)
+
+        val transaction = supportFragmentManager.beginTransaction()
+
+        transaction.add(R.id.fragment_container_view, LoginFragment(), "LoginFragment").commitAllowingStateLoss()
     }
 }
